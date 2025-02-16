8320f8ed95dfeaab8c445d636f63149753681604
Sjoker20060228 <paneljoker2000@gmail.com>
2025-02-16 12:36:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'JK',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Joker V1",
+  botOwner: 'Joker x Savi', // Your name
+  ownerNumber: '94728380062', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
